fc2315e9a76901bdbc32b7fe7dc74ef5062916e6
labubu <miguelramos20100203@gmail.com>
2026-09-22 11:06:35 -0400

    atividade completa com todas as rotas e rota final de tarefas

--- app.js
+++ app.js
@@ -13,8 +13,80 @@ const produtos = [
   { id: 4, nome: "Processador AMD Ryzen 7 7500", categoria: "eletrônicos" },
 ];
 
+const tarefas = [
+  {id: 5, titulo: "Estudar PTAS", concluida: false },
+  {id: 6, titulo: "Prova de portugues",  concluida: true},
+  {id: 7, titulo: "Atividade de quimica",  concluida: false },
+];
+
+function verificarTarefaExiste(req, res, next) {
+  const idBusca = parseInt(req.params.id);
+  const tarefa = tarefas.find(t => t.id === idBusca);
+
+  if (!tarefa) {
+   
+    return res.status(404).json({ erro: 'Tarefa não encontrada.' });
+  }
+
+  req.tarefa = tarefa; 
+  
+  
+  next(); 
+}
+
+app.get('/tarefas/:id', verificarTarefaExiste, (req, res) => {
+
+  res.json(req.tarefa);
+});
+
+const filtrarTarefas = (req, res, next) => {
+  const { concluida } = req.query;
+
+  if (concluida !== undefined) {
+  
+    const statusBuscado = concluida === 'true';
+   
+    req.filtrarTarefas = tarefas.filter(t => t.concluida === statusBuscado);
+  } else {
+   
+    req.filtrarTarefas = tarefas;
+  }
+
+  next();
+};
+
+app.post('/tarefas', (req, res) => {
+const { titulo} = req.body;
+
+ if (!titulo) {
+        return res.status(400).json({ erro: 'O título é obrigatório.' });
+    }
+
+    const novaTarefa = {
+        id: tarefas.length + 1,
+        titulo: titulo,
+        concluida: false
+    };
+
+    tarefas.push(novaTarefa);
+
+    return res.status(201).json(novaTarefa);
+} )
+
+app.get('/tarefas', filtrarTarefas, (req, res) => {
+ 
+  res.json(req.filtrarTarefas);
+});
+
+app.post('/tarefas', [ verificarTarefaExiste], [filtrarTarefas], (req, res)=>{
+   res.status(201).json({
+      mensagem: 'Tarefa criada com sucesso',
+      tarefa: req.body
+})
+}); 
+
 app.get("/", (req, res) => {
-  res.send("Olá Express!");
+  res.json("API de tarefas no ar");
 });
 
 app.get("/produtos", (req, res) => {
@@ -50,9 +122,8 @@ app.post("/produto", (req, res) => {
   res.status(201).json(novoProduto);
 });
 
-// Req Params, pega um parâmetro na rota e identifica um recurso específico;
 app.get("/produtos/:id", (req, res) => {
-  // req.params sempre retorna strings, por isso se usa parseInt
+ 
   const id = req.params.id;
   const produto = produtos.find((p) => p.id === parseInt(id));
   if (!produto) {
